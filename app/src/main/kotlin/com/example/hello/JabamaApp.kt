44ac845bec66ed3532d2b1ecd23b0a686c11d8e8
saeed heidari <s.heidari@jabama.ir>
2026-01-22 13:41:56 +0330

    feat: Update AndroidManifest, adjust .gitignore, and modify various UI components and data models.

---
+++ app/src/main/kotlin/com/example/hello/JabamaApp.kt
@@ -0,0 +1,23 @@
+package com.example.hello
+
+import android.app.Application
+import android.os.Build
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+
+class JabamaApp : Application(), ImageLoaderFactory {
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .components {
+                if (Build.VERSION.SDK_INT >= 28) {
+                    add(ImageDecoderDecoder.Factory())
+                } else {
+                    add(GifDecoder.Factory())
+                }
+            }
+            .crossfade(true)
+            .build()
+    }
+}
